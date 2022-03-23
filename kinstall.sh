edd36d9507ff9cc3a57e45a98a15a02bb0be0752
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-03-23 20:57:45 +0530

    Update kinstall.sh

--- kinstall.sh
+++ kinstall.sh
@@ -15,7 +15,7 @@ curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
 sudo apt update && sudo apt install -y cargo && cargo install ttyper
-sudo apt install -y neovim fzf translate-shell \
+sudo apt install -y neovim fzf translate-shell xfce4-terminal \
 ranger zathura \
 tree ncdu slock \
 feh sxiv ffmpeg \
