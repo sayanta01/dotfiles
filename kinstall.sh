c1c25b6378eac9fdcba41e6a0a15da957566c9ab
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-05-30 16:07:39 +0530

    Update kinstall.sh

--- kinstall.sh
+++ kinstall.sh
@@ -13,7 +13,7 @@ sudo apt install -y fzf ranger \
 zathura ripgrep bat fd-find nodejs yarnpkg \
 ncdu slock imwheel fonts-jetbrains-mono \
 xwallpaper sxiv ffmpeg aptitude \
-fortune font-viewer \
+fortune font-viewer exiftool \
 synaptic stacer stress \
 speedtest-cli trash-cli yt-dlp transmission-cli \
 bpytop neofetch preload htop \
