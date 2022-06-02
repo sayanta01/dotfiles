87f8f946fdf8c4891b159fe18218ee676024fc55
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-02 18:40:38 +0530

    Update kinstall.sh

--- kinstall.sh
+++ kinstall.sh
@@ -8,7 +8,7 @@ yes '
 curl https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
-sudo apt update && sudo apt install -y cargo && cargo install ttyper
+sudo apt update && sudo apt install -y cargo && cargo install ttyper lsd
 sudo apt install -y fzf ranger \
 zathura ripgrep bat fd-find nodejs yarnpkg \
 ncdu slock imwheel fonts-jetbrains-mono \
