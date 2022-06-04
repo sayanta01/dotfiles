47c7745caf91c466e609d62bd34d23b456c9dbab
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-05 03:57:05 +0530

    Update cmp.lua

--- .config/finalnvim/lua/sayanta/plugins/cmp.lua
+++ .config/finalnvim/lua/sayanta/plugins/cmp.lua
@@ -100,8 +100,7 @@ cmp.setup {
   formatting = {
     fields = { "kind", "abbr", "menu" },
     format = function(entry, vim_item)
-    
-    
+      
 -- Kind icons
       vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
