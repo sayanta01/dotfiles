9bf2fe744b3fe236d7ebbe3f8dd705211436a89d
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-05 02:12:56 +0530

    Update and rename packer.lua to plugins.lua

--- .config/finalnvim/lua/sayanta/plugins.lua
+++ .config/finalnvim/lua/sayanta/plugins.lua
@@ -34,7 +34,7 @@ return packer.startup(function(use)
   use 'tamton-aquib/staline.nvim'
   use "kyazdani42/nvim-web-devicons"
   use "kyazdani42/nvim-tree.lua"  
---  use "navarasu/onedark.nvim"
+  use "navarasu/onedark.nvim"
 --  use "lewis6991/impatient.nvim"
   
 --  use "nvim-telescope/telescope.nvim"
@@ -53,16 +53,16 @@ return packer.startup(function(use)
 --  use "akinsho/toggleterm.nvim"
 
   -- cmp plugins
---  use "hrsh7th/nvim-cmp" -- The completion plugin
---  use "hrsh7th/cmp-nvim-lsp"
---  use "saadparwaiz1/cmp_luasnip" -- snippet completions
---  use "hrsh7th/cmp-buffer" -- buffer completions
---  use "hrsh7th/cmp-path" -- path completions
---  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-nvim-lsp"
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
 
   -- Snippets
---  use "L3MON4D3/LuaSnip" --snippet engine
---  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
   -- LSP
 --  use "neovim/nvim-lspconfig" -- enable LSP
