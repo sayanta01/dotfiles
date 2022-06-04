b0e17b8ed5da746a45f4f66d55052a64184618db
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-04 14:23:58 +0530

    Update launch.sh

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#Terminate already running bar instances
+# Terminate already running bar instances
 killall -q polybar
 
-#Wait until the processes have been shutdown
-while pgrep -u 1000 -x polybar >/dev/null; do sleep 1; done
+# Wait until the processes have been shutdown
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-#Launch bar
+# Launch bar
 polybar example &
