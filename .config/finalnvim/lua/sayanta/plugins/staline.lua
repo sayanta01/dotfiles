20799a662c8751e4ec7787061831463388147e80
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-05 01:15:08 +0530

    Update staline.lua

--- .config/finalnvim/lua/sayanta/plugins/staline.lua
+++ .config/finalnvim/lua/sayanta/plugins/staline.lua
@@ -1,1 +1,48 @@
+require('staline').setup {
+	defaults = {
+		left_separator  = "",
+		right_separator = "",
+		cool_symbol     = " ",      
+		full_path       = true,
+		mod_symbol      = "  ",
+		lsp_client_symbol = " ",
+		line_column     = "%l/%c 並%p% ", 
 
+		fg              = "#000000",  
+		bg              = "none",    
+		inactive_color  = "#303030",
+		inactive_bgcolor = "none",
+		true_colors     = false,       
+		font_active     = "bold,italic",   
+		branch_symbol   = " ",
+	},
+	mode_colors = {
+		n = "#2bbb4f",
+		i = "#986fec",
+		c = "#e27d60",
+		v = "#4799eb", 
+	},
+	mode_icons = {
+		n = " NORMAL",
+		i = " INSERT",
+		c = " COMMAND",
+		v = " VISUAL",   
+		v = " VISUAL",   
+	},
+	sections = {
+		left = { '- ', '-mode', 'left_sep', ' ', 'branch' },
+		mid  = { 'file_name' },
+		right = { 'cool_symbol','right_sep', '-line_column' },
+		
+	},
+	special_table = {
+		NvimTree = { 'NvimTree', ' ' },
+		packer = { 'Packer',' ' },        
+	},
+	lsp_symbols = {
+		Error=" ",
+		Info=" ",
+		Warn=" ",
+		Hint="",
+	},
+}
