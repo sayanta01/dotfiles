1023b4e1b27e3b43ed9d0d67d9ff81699776348e
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-04 22:53:00 +0530

    Update options.lua

--- .config/finalnvim/lua/sayanta/options.lua
+++ .config/finalnvim/lua/sayanta/options.lua
@@ -83,7 +83,6 @@ for _, plugin in pairs(default_plugins) do
    g["loaded_" .. plugin] = 1
 end
 
-
 -- set shada path
 vim.schedule(function()
    vim.opt.shadafile = vim.fn.expand "$HOME" .. "/.local/share/nvim/shada/main.shada"
