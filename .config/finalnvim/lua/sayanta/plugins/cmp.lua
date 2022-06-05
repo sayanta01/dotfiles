45f9b9f631a960253c2bc3ba9597bdf850725cc8
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-05 16:08:11 +0530

    Update cmp.lua

--- .config/finalnvim/lua/sayanta/plugins/cmp.lua
+++ .config/finalnvim/lua/sayanta/plugins/cmp.lua
@@ -58,7 +58,7 @@ cmp.setup {
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-    ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` If you want remove default `<C-y>` mapping.
+    ["<C-y>"] = cmp.config.disable,
     ["<C-e>"] = cmp.mapping {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
