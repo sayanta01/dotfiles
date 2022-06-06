9e6913cbced9fef8afaa7e4dfbda3ef9a26b4550
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-06 22:30:41 +0530

    Update keymaps.lua

--- .config/finalnvim/lua/sayanta/keymaps.lua
+++ .config/finalnvim/lua/sayanta/keymaps.lua
@@ -7,9 +7,9 @@ local keymap = vim.api.nvim_set_keymap
 --Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
---vim.g.maplocalleader = " "
+vim.g.maplocalleader = " "
 
--- Modes
+  -- Modes
 --   normal_mode = "n",
 --   insert_mode = "i",
 --   visual_mode = "v",
@@ -18,6 +18,16 @@ vim.g.mapleader = " "
 --   command_mode = "c",
 
 -- Normal --
+-- Rust
+keymap("n", "<leader>rr", ":RustRun<CR>", opts)
+keymap("n", "<leader>rf", ":RustFmt<CR>", opts)
+
+-- No highlight
+keymap("n", "<ESC>", ":noh<CR>", opts)
+
+-- Copy all
+keymap("n", "<C-c-a>", ":%y+ <CR>", opts)    
+      
 -- Better window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
@@ -34,23 +44,32 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
+-- New buffer    
+keymap("n", "<S-b>", ":enew <CR>", opts)
+
+keymap("n", "<leader>n", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<leader>r", ":NvimTreeRefresh<cr>", opts)
+
 -- Move text up and down
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
 
 -- Insert --
 -- Press jk fast to enter
 keymap("i", "jk", "<ESC>", opts)
 
+
 -- Visual --
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
+keymap("v", "<", "<gv", opts)  --S+< >
 keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-j>", ":m .+1<CR>==", opts) --S+
 keymap("v", "<A-k>", ":m .-2<CR>==", opts)
 keymap("v", "p", '"_dP', opts)
+
 
 -- Visual Block --
 -- Move text up and down
@@ -58,4 +77,15 @@ keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+
+-- Terminal --
+-- Better terminal navigation
+-- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+-- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+-- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+-- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- exit terminal with ESC
+keymap("t", "<C-x>", "<c-\\><c-n>:q!<esc>", opts)
 
