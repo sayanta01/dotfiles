fdb5d27468829e8f050fd256d97ab131719c5420
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-06 18:17:56 +0530

    Update config.h

--- .config/dmenu/config.h
+++ .config/dmenu/config.h
@@ -9,9 +9,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#c8daea", "#0b0f18" },
+	[SchemeSel] = { "#c8daea", "#0772CA" },
+	[SchemeOut] = { "#c8daea", "#94B3D7" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
