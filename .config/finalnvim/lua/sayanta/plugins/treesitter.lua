70293b94115fec0ef288382cf87774d11141f720
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-07 14:35:03 +0530

    Update treesitter.lua

--- .config/finalnvim/lua/sayanta/plugins/treesitter.lua
+++ .config/finalnvim/lua/sayanta/plugins/treesitter.lua
@@ -15,11 +15,9 @@ configs.setup {
     disable = { "" }, 
     additional_vim_regex_highlighting = true,
   },
-  
   indent = { enable = true, disable = { "yaml" } },
   context_commentstring = {
     enable = true,
     enable_autocmd = false,
   },
-  
 }
