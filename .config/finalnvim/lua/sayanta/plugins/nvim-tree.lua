9f7108f5889e81f88de690bcccfdbcab3adf1044
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-07 19:51:03 +0530

    Update nvim-tree.lua

--- .config/finalnvim/lua/sayanta/plugins/nvim-tree.lua
+++ .config/finalnvim/lua/sayanta/plugins/nvim-tree.lua
@@ -1,30 +1,108 @@
--- Following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
+local present, nvimtree = pcall(require, "nvim-tree")
+if not present then
+   return
+end
+
+local options = {
+   filters = {
+      dotfiles = false,
+      exclude = { "custom" },
+   },
+   disable_netrw = true,
+   hijack_netrw = true,
+   open_on_setup = false,
+   ignore_ft_on_setup = { "alpha" },
+   open_on_tab = false,
+   hijack_cursor = true,
+   hijack_unnamed_buffer_when_opening = false,
+   update_cwd = true,
+   update_focused_file = {
+      enable = true,
+      update_cwd = false,
+      ignore_list = {},
+   },
+   diagnostics = {
+    enable = true,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
   },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
+   view = {
+      width = 28,
+      height = 28,
+      hide_root_folder = false,
+      side = "left",
+   },
+   git = {
+      enable = false,
+      ignore = true,
+      timeout = 500,
+   },
+   system_open = {
+    cmd = nil,
+    args = {},
   },
+   trash = {
+    cmd = "trash",
+    require_confirm = true,
+  },
+   actions = {
+      open_file = {
+         quit_on_open = true,
+         resize_window = true,
+         window_picker = {
+            enable = false,
+      },
+     },
+   },
+   renderer = {
+      highlight_git = false,
+      highlight_opened_files = "none",
+
+      indent_markers = {
+         enable = false,
+      },
+      icons = {
+         padding = " ",
+         symlink_arrow = " ➛ ",
+         show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = false,
+         },
+         glyphs = {
+            default = "",
+            symlink = "",
+            folder = {
+               default = "",
+               empty = "",
+               empty_open = "",
+               open = "",
+               symlink = "",
+               symlink_open = "",
+               arrow_open = "",
+               arrow_closed = "",
+            },
+            git = {
+               unstaged = "✗",
+               staged = "✓",
+               unmerged = "",
+               renamed = "➜",
+               untracked = "U",
+               deleted = "",
+               ignored = "◌",
+            },
+         },
+      },
+   },
 }
 
-local status_ok, nvim_tree = pcall(require, "nvim-tree")
-if not status_ok then
-  return
-end
+-- check for any override
+nvimtree.setup(options)
 
 local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
 if not config_status_ok then
@@ -42,76 +120,3 @@ vim.api.nvim_create_autocmd("BufEnter", {
   end
 })
 
-nvim_tree.setup {
-  disable_netrw = true,
-  hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = { "alpha" },
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = true,
-  diagnostics = {
-    enable = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-    ignore_list = {},
-  },
-  system_open = {
-    cmd = nil,
-    args = {},
-  },
-  filters = {
-    dotfiles = false,
-    custom = {},
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  view = {
-    width = 26,
-    height = 26,
-    hide_root_folder = false,
-    side = "left",
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
-    number = false,
-    relativenumber = false,
-  },
-  trash = {
-    cmd = "trash",
-    require_confirm = true,
-  },
-  actions = {
-    open_file = {
-      quit_on_open = true,
-      window_picker = {
-            enable = false,
-      },
-    },
-  },
-
---  unknown options as of 22.05
---  update_to_buf_dir = {
---    enable = true,
---    auto_open = true,
---  },
---  git_hl = 1,
---  auto_resize = true,
---  root_folder_modifier = ":t",
-}
