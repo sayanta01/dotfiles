77b5904dd75d5e68df98a12952dc29931f18d4fc
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-07 21:34:40 +0530

    Update init.lua

--- .config/finalnvim/init.lua
+++ .config/finalnvim/init.lua
@@ -1,7 +1,7 @@
 -- For Null-LS formatting
 --pip3 install pynvim black flake8
 --cargo install stylua
---sudo apt install -y ueberzug eslint 
+--sudo apt install -y ueberzug eslint golang-go gccgo-go
 --sudo npm install -g prettier
 
 require "options"
