a926ce20f9e4da3a6adb6051962b83397c12239b
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-08 20:14:07 +0530

    Update indentline.lua

--- .config/nvim/lua/plugins/indentline.lua
+++ .config/nvim/lua/plugins/indentline.lua
@@ -13,9 +13,10 @@ vim.g.indent_blankline_filetype_exclude = {
 	"NvimTree",
 	"Trouble",
 }
--- vim.g.indent_blankline_char = "│"
-vim.g.indent_blankline_char = "▏"
+--vim.g.indentLine_enabled = 1
+--vim.g.indent_blankline_char = "│"
 -- vim.g.indent_blankline_char = "▎"
+vim.g.indent_blankline_char = "▏"
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 vim.g.indent_blankline_show_first_indent_level = true
 vim.g.indent_blankline_use_treesitter = true
@@ -61,13 +62,10 @@ indent_blankline.setup({
 	-- space_char_blankline = " ",
 	show_current_context = true,
 	-- show_current_context_start = true,
-	 --char_highlight_list = {
-       --"IndentBlanklineIndent1",
-       --"IndentBlanklineIndent2",
-       --"IndentBlanklineIndent3",
-       --"IndentBlanklineIndent4",
-       --"IndentBlanklineIndent5",
-       --"IndentBlanklineIndent6",
+	-- char_highlight_list = {
+	--   "IndentBlanklineIndent1",
+	--   "IndentBlanklineIndent2",
+	--   "IndentBlanklineIndent3",
 	-- },
 })
 
