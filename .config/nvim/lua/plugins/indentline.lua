1aa80a39cae41666f1e2c9babc0f2d5b323bc007
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-09 13:10:24 +0530

    Update indentline.lua

--- .config/nvim/lua/plugins/indentline.lua
+++ .config/nvim/lua/plugins/indentline.lua
@@ -13,10 +13,10 @@ vim.g.indent_blankline_filetype_exclude = {
 	"NvimTree",
 	"Trouble",
 }
---vim.g.indentLine_enabled = 1
-vim.g.indent_blankline_char = "│"
+--vim.g.indent_blankline_char = "│"
 --vim.g.indent_blankline_char = "▎"
 --vim.g.indent_blankline_char = "▏"
+vim.g.indent_blankline_char = ""
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 vim.g.indent_blankline_show_first_indent_level = true
 vim.g.indent_blankline_use_treesitter = true
@@ -54,13 +54,12 @@ vim.wo.colorcolumn = "99999"
 -- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
 -- vim.opt.list = true
 -- vim.opt.listchars:append "space:⋅"
--- vim.opt.listchars:append "space:"
 -- vim.opt.listchars:append "eol:↴"
 
-indent_blankline.setup({
+--indent_blankline.setup({
 	-- show_end_of_line = true,
 	-- space_char_blankline = " ",
-	show_current_context = true,
+	-- show_current_context = true,
 	-- show_current_context_start = true,
         --char_highlight_list = {
         --"IndentBlanklineIndent1",
@@ -70,5 +69,5 @@ indent_blankline.setup({
         --"IndentBlanklineIndent5",
         --"IndentBlanklineIndent6",
 	-- },
-})
+--})
 
