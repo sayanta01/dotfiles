2748a48944a8f279906bdf0fe15f108fcc8c1450
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-13 05:05:57 +0530

    Update indentline.lua

--- .config/nvim/lua/plugins/indentline.lua
+++ .config/nvim/lua/plugins/indentline.lua
@@ -3,7 +3,7 @@ if not status_ok then
 	return
 end
 
-vim.g.indent_blankline_buftype_exclude = { "terminal" }
+vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
 vim.g.indent_blankline_filetype_exclude = {
 	"help",
 	--"startify",
