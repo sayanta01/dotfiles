1f8e8246b6a04196e3ca542b1bd45df10667e54f
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-13 05:16:13 +0530

    Update colorscheme.lua

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,5 @@ local colorscheme = "tokyonight"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. "not found!")
 	return
 end
