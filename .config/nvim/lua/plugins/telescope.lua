d929369984902e862fb629bb71dfcbff567e227f
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-13 05:48:10 +0530

    Update telescope.lua

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -41,7 +41,6 @@ telescope.setup {
         ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
         ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
         ["<C-l>"] = actions.complete_tag,
-        --["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
       },
 
       n = {
