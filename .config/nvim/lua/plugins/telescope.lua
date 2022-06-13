e5e43f207fab065ee18200292ab6ab6448546275
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-13 05:38:08 +0530

    Update telescope.lua

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -41,7 +41,7 @@ telescope.setup {
         ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
         ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
         ["<C-l>"] = actions.complete_tag,
-       --["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+        --["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
       },
 
       n = {
@@ -73,7 +73,7 @@ telescope.setup {
         ["<PageUp>"] = actions.results_scrolling_up,
         ["<PageDown>"] = actions.results_scrolling_down,
 
-        --["?"] = actions.which_key,
+        ["?"] = actions.which_key,
       },
     },
   },
