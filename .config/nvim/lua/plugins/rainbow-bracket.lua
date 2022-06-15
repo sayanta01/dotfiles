9049018cba7cba8944706fb5c14a59455aaef2cb
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-15 17:39:15 +0530

    Update rainbow-bracket.lua

--- .config/nvim/lua/plugins/rainbow-bracket.lua
+++ .config/nvim/lua/plugins/rainbow-bracket.lua
@@ -5,7 +5,7 @@ require("nvim-treesitter.configs").setup {
     enable = true,
     -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    max_file_lines = 999, -- Do not enable for files with more than n lines, int
     --colors = {}, -- table of hex strings
     --termcolors = {} -- table of colour name strings
   }
