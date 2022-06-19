343e2733da44a3bccde78f1c67a03933705c32cf
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-19 06:15:58 +0530

    Add files via upload

---
+++ .config/nvim/lua/user/dap.lua
@@ -0,0 +1,37 @@
+local dap_status_ok, dap = pcall(require, "dap")
+if not dap_status_ok then
+  return
+end
+
+local dap_ui_status_ok, dapui = pcall(require, "dapui")
+if not dap_ui_status_ok then
+  return
+end
+
+local dap_install_status_ok, dap_install = pcall(require, "dap-install")
+if not dap_install_status_ok then
+  return
+end
+
+dap_install.setup {
+  installation_path = vim.fn.stdpath "data" .. "/dapinstall/",
+}
+
+dap_install.config("python", {})
+-- add other configs here
+
+dapui.setup {}
+
+vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
