027173d7fe06642bf052d54c5a0707d063212e82
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-23 23:26:59 +0530

    Delete .config/nvim directory

--- .config/nvim/lua/user/colorizer.lua
+++
@@ -1,4 +0,0 @@
-local options = { rgb_fn = true, hsl_fn = true, css = true, css_fn = true, names = true, RGB = true, RRGGBB = true, RRGGBBAA = true}
-require("colorizer").setup({
-  "*",
-}, options)
