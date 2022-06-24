5c592948b6381a5ebda62b1cd021d78ff60cee78
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-24 23:25:23 +0530

    Update kinstall.sh

--- kinstall.sh
+++ kinstall.sh
@@ -53,20 +53,6 @@ wget 'https://discord.com/api/download?platform=linux&format=deb' -O ~/Downloads
 cd ~/Downloads && chmod +x vscode.deb discord.deb && sudo dpkg -i vscode.deb discord.deb
 
 git clone https://github.com/abba23/spotify-adblock.git && cd spotify-adblock && make && sudo make install
-touch ~/.local/share/applications/spotify-adblock.desktop
-cat > spotify-adblock.desktop <<EOF
-[Desktop Entry]
-Type=Application
-Name=Spotify (adblock)
-GenericName=Music Player
-Icon=spotify-client
-TryExec=spotify
-Exec=env LD_PRELOAD=/usr/local/lib/spotify-adblock.so spotify %U
-Terminal=false
-MimeType=x-scheme-handler/spotify;
-Categories=Audio;Music;Player;AudioVideo;
-StartupWMClass=spotify
-EOF
    
 sudo cat <<EOF | sudo tee /etc/resolv.conf 
 nameserver 1.1.1.1
