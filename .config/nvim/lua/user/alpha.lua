049ca0d00987853d3558938b4c89e487f316cf0f
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-27 02:16:11 +0530

    Update alpha.lua

--- .config/nvim/lua/user/alpha.lua
+++ .config/nvim/lua/user/alpha.lua
@@ -22,7 +22,7 @@ dashboard.section.buttons.val = {
   dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
   dashboard.button("b", "  Bookmarks ", ":Telescope marks<CR>"),
   dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
-  dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+  dashboard.button("r", "󱑅 " .. " Recent files", ":Telescope oldfiles <CR>"),
   dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
   dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
   dashboard.button("q", " " .. " Quit", ":qa<CR>"),
