92aa8dbb80fa0e46e293dc65e9aad7b58706db60
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-06-27 01:42:20 +0530

    Update kinstall.sh

--- kinstall.sh
+++ kinstall.sh
@@ -15,7 +15,7 @@ cp -r ~/Desktop/dotfiles/{.zshrc,.local,.fonts,.xinitrc,.imwheelrc,.zprofile,.ic
 cp -r ~/Desktop/dotfiles/.config/[^.]* ~/.config
 chmod +x ~/.config/bspwm/bspwmrc ~/.config/polybar/launch.sh ~/.local/bin/* ~/.config/sxiv/exec/key-handler
 
-git clone https://github.com/LukeSmithxyz/dmenu.git ~/.Downloads
+git clone https://github.com/LukeSmithxyz/dmenu.git ~/.Downloads/dmenu
 cd ~/.Donwloads/dmenu && sudo make clean install
 #cd ~/.config/dmenu && sudo make clean install
 
