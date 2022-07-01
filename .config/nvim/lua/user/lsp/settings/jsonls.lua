22aab7b9e5d330e11152c01a846b14d47e678cd9
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-07-01 12:14:05 +0530

    Update jsonls.lua

--- .config/nvim/lua/user/lsp/settings/jsonls.lua
+++ .config/nvim/lua/user/lsp/settings/jsonls.lua
@@ -1,24 +1,18 @@
-local status_ok, schemastore = pcall(require, "schemastore")
-if not status_ok then
-  return
-end
-
-return {
-  init_options = {
-    provideFormatter = false,
-  },
+local opts = {
   settings = {
     json = {
-      schemas = schemastore.json.schemas(),
+      schemas = require("schemastore").json.schemas(),
     },
   },
   setup = {
     commands = {
-      -- Format = {
-      --   function()
-      --     vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line "$", 0 })
-      --   end,
-      -- },
+      Format = {
+        function()
+          vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line "$", 0 })
+        end,
+      },
     },
   },
 }
+
+return opts
