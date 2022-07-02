ea998aad335f95deb1573bd08c5a889de978e20e
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-07-03 02:33:08 +0530

    Update kinstall.sh

--- kinstall.sh
+++ kinstall.sh
@@ -53,7 +53,7 @@ telegram-desktop deluge docker.io
 cargo install ttyper lsd bat
 
 sudo apt purge -y --auto-remove onboard xterm cherrytree \
-xfce4-power-manager xfce4-notifyd  xfce4-screensaver xfce4-clipman-plugin
+xfce4-notifyd  xfce4-screensaver xfce4-clipman-plugin
 
 # Build Neovim
 sudo apt install -y ninja-build gettext libtool libtool-bin cmake pkg-config doxygen
