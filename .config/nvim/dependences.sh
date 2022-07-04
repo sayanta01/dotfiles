6f017b6571bb7da93bb456e660ca0b0391940d2d
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-07-05 00:11:33 +0530

    Update dependences.sh

--- .config/nvim/dependences.sh
+++ .config/nvim/dependences.sh
@@ -3,4 +3,5 @@
 pip3 install pynvim black flake8 
 cargo install stylua
 sudo npm i -g prettier astyle truecallerjs emmet html tailwindcss live-server
-sudo  ueberzug eslint ccls shfmt shellcheck luacheck
+#sudo apt install -y ueberzug eslint ccls shfmt shellcheck luacheck
+#sudo pacman -S --noconfirm ueberzug eslint ccls shfmt shellcheck luacheck
