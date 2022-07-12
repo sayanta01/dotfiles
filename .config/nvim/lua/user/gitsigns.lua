a70b56d72e10fd464e48109e47df96bb02265739
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-07-12 19:35:42 +0530

    Create gitsigns.lua

---
+++ .config/nvim/lua/user/gitsigns.lua
@@ -0,0 +1,27 @@
+local status_ok, gitsigns = pcall(require, "gitsigns")
+if not status_ok then
+  return
+end
+
+gitsigns.setup {
+  signs = {
+    add = { hl = "GitSignsAdd", text = "", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+    change = { hl = "GitSignsChange", text = "", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    delete = { hl = "GitSignsDelete", text = "", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    topdelete = { hl = "GitSignsDelete", text = "", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+  },
+  numhl = false,
+  linehl = false,
+  keymaps = {
+        -- Default keymap options
+        noremap = true,
+        buffer = true,
+      },
+  watch_gitdir = {
+  interval = 1000
+  },
+  sign_priority = 6,
+  update_debounce = 200,
+  status_formatter = nill
+}
