a371245e2ac29d9e792549f9750f0c67064e1a2e
Sayanta <68754658+sayanta01@users.noreply.github.com>
2022-07-28 23:58:10 +0530

    Update indentline.lua

--- .config/nvim/lua/user/indentline.lua
+++ .config/nvim/lua/user/indentline.lua
@@ -21,7 +21,7 @@ vim.g.indent_blankline_filetype_exclude = {
 vim.g.indent_blankline_char = ":"
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 vim.g.indent_blankline_show_first_indent_level = true
-vim.g.indent_blankline_show_current_context = false
+vim.g.indent_blankline_show_current_context = true
 vim.g.indent_blankline_use_treesitter = true
 vim.g.indent_blankline_context_patterns = {
 	"class",
