c9a0c05be427cbc584a78f47ab298e0fef8d6d3f
sayanta <sayantasingh01@gmail.com>
2023-03-27 23:13:42 +0530

    update

--- .config/mpv/scripts/live-filters.lua
+++ .config/mpv/scripts/live-filters.lua
@@ -21,6 +21,14 @@
 -- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 -- IN THE SOFTWARE.
 
+
+
+
+--[[ Shift + w ]]
+
+
+
+
 local utils = require("mp.utils")
 local options = require("mp.options")
 local assdraw = require("mp.assdraw")
