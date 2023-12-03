b9bc0adeff40fc3a28eab495cf0784d1a78fcbf3
sayanta <sayantasingh01@gmail.com>
2023-12-03 22:58:25 +0530

    update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,18 +6,17 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u "$(id -u)" -x polybar >/dev/null; do sleep 1; done
 
-desktop=$DESKTOP_SESSION
-# count=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)
+polybar example &
 
-case $desktop in
-
-bspwm | /usr/share/xsessions/bspwm)
-	if type "xrandr" >/dev/null; then
-		for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-			MONITOR=$m polybar --reload example -c ~/.config/polybar/config.ini &
-		done
-	else
-		polybar --reload example -c ~/.config/polybar/config.ini &
-	fi
-	;;
-esac
+# desktop=$DESKTOP_SESSION
+# case $desktop in
+# bspwm | /usr/share/xsessions/bspwm)
+# 	if type "xrandr" >/dev/null; then
+# 		for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+# 			MONITOR=$m polybar --reload example -c ~/.config/polybar/config.ini &
+# 		done
+# 	else
+# 		polybar --reload example -c ~/.config/polybar/config.ini &
+# 	fi
+# 	;;
+# esac
