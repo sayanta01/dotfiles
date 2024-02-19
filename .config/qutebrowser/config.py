ca18053add8d5a365b9a3a41617b7cd6b9d1cea1
sayanta <sayantasingh01@gmail.com>
2024-02-19 11:06:38 +0530

    update

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -3,7 +3,7 @@ config.set("colors.webpage.darkmode.enabled", True)
 
 # Bindings for normal mode
 config.bind('M', 'hint links spawn mpv {hint-url}')
-config.bind('Z', 'hint links spawn st -e yt-dlp {hint-url}')
+config.bind('Z', 'hint links spawn kitty -e yt-dlp {hint-url}')
 config.bind('t', 'set-cmd-text -s :open -t')
 config.bind('xt', 'config-cycle tabs.show always never')
 config.bind('xx', 'config-cycle statusbar.show always never;; config-cycle tabs.show always never')
