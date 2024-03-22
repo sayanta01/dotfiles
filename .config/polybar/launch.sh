0c20003967ad4e4a9cba3e01ac78a74416f05f91
sayanta <sayantasingh01@gmail.com>
2024-03-23 00:03:31 +0530

    update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,15 +8,15 @@ while pgrep -u "$(id -u)" -x polybar >/dev/null; do sleep 1; done
 
 polybar example &
 
-desktop=$DESKTOP_SESSION
-case $desktop in
-bspwm | /usr/share/xsessions/bspwm)
-	if type "xrandr" >/dev/null; then
-		for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-			MONITOR=$m polybar --reload example &
-		done
-	else
-		polybar --reload example &
-	fi
-	;;
-esac
+# desktop=$DESKTOP_SESSION
+# case $desktop in
+# bspwm | /usr/share/xsessions/bspwm)
+# 	if type "xrandr" >/dev/null; then
+# 		for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+# 			MONITOR=$m polybar --reload example &
+# 		done
+# 	else
+# 		polybar --reload example &
+# 	fi
+# 	;;
+# esac
