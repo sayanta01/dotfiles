0c5fe411c230b180da7ebbc0f50dd27a06fac768
sayanta <sayantasingh01@gmail.com>
2024-07-27 18:03:18 +0530

    update

--- .config/mpv/scripts/thumbfast.lua
+++ .config/mpv/scripts/thumbfast.lua
@@ -2,12 +2,12 @@
 --
 -- High-performance on-the-fly thumbnailer
 --
--- Built for easy integration in third-party UIs
+-- Built for easy integration in third-party UIs.
 
 --[[
 This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
-file, You can obtain one at https://mozilla.org/MPL/2.0/
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
 ]]
 
 local options = {
