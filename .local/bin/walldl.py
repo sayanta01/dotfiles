210ed2e3235bbcd5720f0f91994bb938e53499cf
sayanta <sayantasingh01@gmail.com>
2024-09-09 14:46:52 +0530

    update

--- .local/bin/walldl.py
+++ .local/bin/walldl.py
@@ -16,7 +16,6 @@ DOWNLOAD_DIR = f"{str(pathlib.Path.home())}/Pictures/wall"
 def generate_id():
     return "".join(random.choices(string.ascii_lowercase + string.digits, k=6))
 
-
 def get_ext(url):
     ext = os.path.splitext(url)[1]
     return ext
