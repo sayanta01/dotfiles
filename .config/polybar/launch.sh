d9beb9169daa00eedfb2a8dc2877a47279660f63
sayanta <sayantasingh01@gmail.com>
2025-06-30 21:30:25 +0530

    update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+polybar-msg cmd quit
 
 # Launch
-polybar example &
-
-# desktop=$DESKTOP_SESSION
-# case $desktop in
-# bspwm | /usr/share/xsessions/bspwm)
-# 	if type "xrandr" >/dev/null; then
-# 		for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-# 			MONITOR=$m polybar --reload example &
-# 		done
-# 	else
-# 		polybar --reload example &
-# 	fi
-# 	;;
-# esac
+if command -v xrandr >/dev/null 2>&1; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar example &
+  done
+else
+  polybar example &
+fi
