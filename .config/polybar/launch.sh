2fff86bbcc36091649f3503ca7cdca021e86a03d
sayanta <sayanta01@gmail.com>
2025-08-19 23:57:13 +0530

    update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,10 +4,6 @@
 polybar-msg cmd quit
 
 # Launch
-if command -v xrandr >/dev/null 2>&1; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar example &
-  done
-else
-  polybar example &
-fi
+for m in $(polybar -M | cut -d ':' -f1); do
+  MONITOR=$m polybar example &
+done
